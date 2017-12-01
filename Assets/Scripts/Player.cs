07d50e63a07d18c7b975a7166caf6b26de33946a
yiding he <yidinghe2012@gmail.com>
2017-11-30 21:23:07 -0500

    Finished Player Script.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+	
+	public float speed = 8f;
+	public float maxVelocity = 4f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake ()
+	{
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		playerMoveKeyBoard ();
+	}
+
+	void playerMoveKeyBoard ()
+	{
+		float forceX = 0f;
+		float vel = Mathf.Abs (myBody.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (h > 0) {
+			//Going Right
+			if (vel < maxVelocity)
+				forceX = speed;
+
+			Vector3 temp = transform.localScale;
+			temp.x = 1.3f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+		} else if (h < 0) {
+			//Going left
+			if (vel < maxVelocity)
+				forceX = -speed;
+
+			Vector3 temp = transform.localScale;
+			temp.x = -1.3f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+			
+		myBody.AddForce (new Vector2(forceX, 0));
 	}
 }
