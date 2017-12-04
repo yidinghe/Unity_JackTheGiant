f21fd58b59478fecde788d436aa9b6de74fd2462
yiding he <yiding.he@samsungpay.com>
2017-12-04 14:27:53 -0500

    Finished CameraScript.

--- Assets/Scripts/Camera Scripts/CameraScript.cs
+++ Assets/Scripts/Camera Scripts/CameraScript.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour {
+public class CameraScript : MonoBehaviour
+{
+
+	private float speed = 1f;
+	private float acceleration = 0.2f;
+	private float maxSpeed = 3.2f;
+
+	[HideInInspector]
+	public bool moveCamera;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		moveCamera = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (moveCamera) {
+			MoveCamera ();
+		}
 	}
+
+	void MoveCamera ()
+	{
+		Vector3 temp = transform.position;
+		float oldY = temp.y;
+		float newY = temp.y - (speed * Time.deltaTime);
+		temp.y = Mathf.Clamp (temp.y, oldY, newY);
+		transform.position = temp;
+		speed += acceleration * Time.deltaTime;
+		if (speed > maxSpeed)
+			speed = maxSpeed;
+	}
+
 }
