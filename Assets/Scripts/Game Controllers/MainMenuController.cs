8c764844950b00982aad5f9d2fc1818d9eaf9c3e
yiding he <yidinghe2012@gmail.com>
2017-12-05 21:31:50 -0500

    Added Navigation button Scripts.

---
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void StartGame(){
+		SceneManager.LoadScene ("GamePlay");
+	}
+
+	public void HighscoreMenu(){
+		SceneManager.LoadScene ("HighScore");
+	}
+
+	public void OptionsMenu(){
+		SceneManager.LoadScene ("OptionsMenu");
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+
+
+	public void MusicButton(){
+
+	}
+}
---
+++ Assets/Scripts/Game Controllers/OptionsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void GoBackToMainMenu(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+}
