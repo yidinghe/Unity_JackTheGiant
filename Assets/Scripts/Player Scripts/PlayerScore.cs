0e9b936965c04df53ba36bcd9932539569c07073
yiding he <yidinghe2012@gmail.com>
2017-12-05 22:00:39 -0500

    Added PlayerScore Script.

---
+++ Assets/Scripts/Player Scripts/PlayerScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip coinClip, lifeClip;
+
+	private CameraScript cameraScript;
+
+	private Vector3 previousPosition;
+	private bool countScore;
+
+	public static int scoreCount;
+	public static int lifeScore;
+	public static int coinScore;
+
+	void Awake(){
+		cameraScript = Camera.main.GetComponent<CameraScript> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		previousPosition = transform.position;
+		countScore = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
