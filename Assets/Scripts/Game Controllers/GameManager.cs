ce40aa083967ec78a906cbac5fbdf4c391f9d98c
yiding he <yidinghe2012@gmail.com>
2017-12-06 22:19:40 -0500

    Added GameManager Script.

---
+++ Assets/Scripts/Game Controllers/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	[HideInInspector]
+	public bool gameStartedFromMainMenu, gameRestartedAfterPlayerDied;
+
+	[HideInInspector]
+	public int score, coinScore, lifeScore;
+
+	void OnEnable() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnDisable() {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (scene.name == "Gameplay") {
+			if (gameRestartedAfterPlayerDied) {
+				
+				GamePlayController.instance.SetScore (score);
+				GamePlayController.instance.SetCoinScore (coinScore);
+				GamePlayController.instance.SetLifeScore (lifeScore);
+
+				PlayerScore.scoreCount = score;
+				PlayerScore.coinCount = coinScore;
+				PlayerScore.lifeCount = lifeScore;
+
+			} else if (gameStartedFromMainMenu) {
+				PlayerScore.scoreCount = 0;
+				PlayerScore.coinCount = 0;
+				PlayerScore.lifeCount = 2;
+			}
+				
+		}
+	}
+
+	void Awake(){
+		MakeSingleton ();
+	}
+
+	void MakeSingleton(){
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+}
